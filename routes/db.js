7e1a36c93ed5820a94ee190058bf56fe57419707
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2018-12-28 12:32:30 +0100

    Db config

--- routes/db.js
+++ routes/db.js
@@ -1,36 +1,13 @@
 var mysql = require('mysql');
-
-var db_config = {
+var connection = mysql.createConnection({
     host     : 'ftp.b-project.mg',
     user     : 'bproject_balzacLeGeek',
     password : '2ypAR/#$39x',
     database : 'bproject_mglocalisation'
-};
+});
 
-var connection;
-
-function handleDisconnect() {
-    console.log('1. connecting to db:');
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-													// the old one cannot be reused.
-
-    connection.connect(function(err) {              	// The server is either down
-        if (err) {                                     // or restarting (takes a while sometimes).
-            console.log('2. error when connecting to db:', err);
-            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
-        }                                     	// to avoid a hot loop, and to allow our node script to
-    });                                     	// process asynchronous requests in the meantime.
-    											// If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-        console.log('3. db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
-            handleDisconnect();                      	// lost due to either server restart, or a
-        } else {                                      	// connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
-
-handleDisconnect();
+connection.connect(function(err) {
+    if (err) throw err;
+});
 
 module.exports = connection;
