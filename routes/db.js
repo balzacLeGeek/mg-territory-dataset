44fb269ad2fe97e2970b4d38589c59794441f1d0
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2018-12-28 11:51:22 +0100

    Setting up bproject Database acces

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : '',
-    database : 'petit_metiers'
+    host     : 'ftp.b-project.mg',
+    user     : 'bproject_balzacLeGeek',
+    password : '2ypAR:#$39x',
+    database : 'bproject_mglocalisation'
 });
 
 connection.connect(function(err) {
