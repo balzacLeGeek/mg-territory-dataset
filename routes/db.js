e54d9cf4b17781cd35cb80f135986c90dd9e691a
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2018-12-28 12:15:18 +0100

    Editing DB Mdp

--- routes/db.js
+++ routes/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'ftp.b-project.mg',
     user     : 'bproject_balzacLeGeek',
-    password : '2ypAR:#$39x',
+    password : '2ypAR/#$39x',
     database : 'bproject_mglocalisation'
 });
 
