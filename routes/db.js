e2fd1beb5dfc9f2359eede437f20e8129b3bdf79
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2018-12-28 13:13:13 +0100

    Heroku Database

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'ftp.b-project.mg',
-    user     : 'bproject_balzacLeGeek',
-    password : '2ypAR/#$39x',
-    database : 'bproject_mglocalisation'
+    host     : 'us-cdbr-iron-east-01.cleardb.net',
+    user     : 'b64a6918b92cd1',
+    password : '3adb58b3',
+    database : 'heroku_4aa6e1b95f5c2bf'
 });
 
 connection.connect(function(err) {
